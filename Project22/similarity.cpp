49c2e46edafd20f0f0dac5ced5e650c93c76e742
monica kang <aksenwkd92@naver.com>
2025-05-07 10:27:35 +0900

    [refactoring] Similarity 매직넘버 추가 및 메서드 추출

--- Project22/similarity.cpp
+++ Project22/similarity.cpp
@@ -4,19 +4,22 @@
 using std::string;
 using std::vector;
 
+const int MAX_LEN_POINT = 60;
+const int MIN_LEN_POINT = 0;
+
 class Similarity {
 public:
 	int checkLength(string input1, string input2) {
 		int len1 = input1.length();
 		int len2 = input2.length();
-		if (len1 == len2) return 60;
-		if (len1 == len2 * 2 || len1 * 2 == len2) return 0;
+		if (len1 == len2) return MAX_LEN_POINT;
+		if (len1 == len2 * 2 || len1 * 2 == len2) return MIN_LEN_POINT;
 
 		 double gap, min;
 		if (len1 > len2) {
 			gap = len1 - len2;
 			min = len2;
-			
+			return (1 - static_cast<double>(len1 - len2) / len2) * 60;
 		}
 		else {
 			gap = len2 - len1;
