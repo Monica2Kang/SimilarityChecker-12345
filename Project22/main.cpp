c88219501a5b17085ec376c88fc7b54781cc5fda
monica kang <aksenwkd92@naver.com>
2025-05-07 10:15:56 +0900

    [feature] TestCase 3개 추가 구현

--- Project22/main.cpp
+++ Project22/main.cpp
@@ -10,6 +10,33 @@ TEST(TS, Length1) {
 	EXPECT_EQ(expected, actual);
 }
 
+TEST(TS, Length2) {
+	Similarity sim;
+	string str1 = "A";
+	string str2 = "BB";
+	int actual = sim.checkLength(str1, str2);
+	int expected = 0;
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TS, Length3) {
+	Similarity sim;
+	string str1 = "AAABB";
+	string str2 = "BAA";
+	int actual = sim.checkLength(str1, str2);
+	int expected = 20;
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TS, Length4) {
+	Similarity sim;
+	string str1 = "AA";
+	string str2 = "AAE";
+	int actual = sim.checkLength(str1, str2);
+	int expected = 30;
+	EXPECT_EQ(expected, actual);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
--- Project22/similarity.cpp
+++ Project22/similarity.cpp
@@ -12,15 +12,16 @@ public:
 		if (len1 == len2) return 60;
 		if (len1 == len2 * 2 || len1 * 2 == len2) return 0;
 
-		int gap, min;
+		 double gap, min;
 		if (len1 > len2) {
 			gap = len1 - len2;
 			min = len2;
+			
 		}
 		else {
 			gap = len2 - len1;
 			min = len1;
 		}
-		return (1 - gap / min) * 60;
+		return (1- gap / min) * 60;
 	}
 };
